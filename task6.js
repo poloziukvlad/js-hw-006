b63ce24af17cd0508d1265904876893b06a111b9
poloziukvlad <polozettix@gmail.com>
2024-06-18 18:04:07 +0300

    added task6

---
+++ task6.js
@@ -0,0 +1,14 @@
+// Комбіноване приведення типів
+// Створіть програму, яка виконує наступні дії:
+
+// Створює зміну inputValue і присвоює їй будь-яке значення (наприклад, число, рядок або булеве значення).
+// Виводить на екран тип inputValue.
+// Приводить inputValue до рядка і виводить результат на екран.
+// Приводить inputValue до числа і виводить результат на екран.
+// Приводить inputValue до булевого типу і виводить результат на екран.
+
+let inputValue = 55;
+console.log(typeof inputValue);
+console.log(String(inputValue));
+console.log(Number(inputValue));
+console.log(Boolean(inputValue));
