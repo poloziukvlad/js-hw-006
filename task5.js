89642bc98da3b97fba1ccb567f95790501e17b3b
poloziukvlad <polozettix@gmail.com>
2024-06-18 17:58:48 +0300

    added task5

---
+++ task5.js
@@ -0,0 +1,11 @@
+// Створіть програму, яка виконує наступні дії:
+
+// Створює зміну inputValue і присвоює їй значення "true".
+// Приводить inputValue до булевого типу (boolean) і присвоює результат змінній isTrue.
+// Виводить isTrue на екран.
+
+let inputValue = "true";
+
+let isTrue = Boolean(inputValue);
+
+console.log(isTrue);
