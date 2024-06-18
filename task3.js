d64c352764f9a6e3a991a2c5952a72d0054acbcd
poloziukvlad <polozettix@gmail.com>
2024-06-18 17:47:49 +0300

    added task3

---
+++ task3.js
@@ -0,0 +1,12 @@
+// Приведення булевих значень
+// Створіть програму, яка виконує наступні дії:
+
+// Створює зміну isLoggedOut і присвоює їй значення false.
+// Створює зміну loggedInString і приводить isLoggedOut до рядка (string), що містить слово "false".
+// Виводить loggedInString на екран.
+
+let isLoggedOut = false;
+
+let loggedInString = String(isLoggedOut);
+
+console.log(loggedInString);
