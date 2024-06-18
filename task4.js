897f3dc275b9d22ebfaefeec295c485a3c5134d0
poloziukvlad <polozettix@gmail.com>
2024-06-18 17:52:43 +0300

    added task4

---
+++ task4.js
@@ -0,0 +1,13 @@
+// Створіть програму, яка виконує наступні дії:
+
+// Створює зміну ageString і присвоює їй рядок "25".
+// Створює зміну yearsToAdd і присвоює їй число 10.
+// Приводить ageString та yearsToAdd до чисел, а потім обчислює суму цих чисел і присвоює результат змінній futureAge.
+// Виводить на екран futureAge.
+
+let ageString = "42";
+let yearsToAdd = 10;
+
+let futureAge;
+futureAge = Number(ageString) + yearsToAdd;
+console.log(futureAge);
